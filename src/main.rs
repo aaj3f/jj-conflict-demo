dcb0f9981398b991b4ba4715f3c3ede0aef12fa5
Andrew Johnson <ajohnson@flur.ee>
2025-12-10 14:37:17 -0500

    impl clap in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "greeting")]
+#[command(about = "A simple greeting application")]
+struct Args {
+    // No arguments yet
+}
+
 fn main() {
+    let _args = Args::parse();
     println!("Hello, world!");
 }
